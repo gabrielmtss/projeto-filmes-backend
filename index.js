f61484f84a7caa21d7147c0cd8ca048a1c63f5cc
Gabriel Matos <g.matos@outlook.com>
2021-12-10 21:01:55 -0300

    configura index

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}.`)
+});
