1a14ded1c0a54cf81c2d909b5070de4fd1141bc3
Gabriel Matos <g.matos@outlook.com>
2021-12-12 12:42:34 -0300

    correção de funções em filmes.controller

--- controllers/filmes.controller.js
+++ controllers/filmes.controller.js
@@ -11,49 +11,44 @@ const getFilme = async (req, res) => {
 }
 
 const addFilme = async (req, res) => {
-  if(!req.body || !req.body.nome || !req.body.iamgem || !req.body.genero || !req.body.nota || !req.body.assistido) {
+  if(!req.body || !req.body.nome || !req.body.imagem || !req.body.genero || !req.body.nota || !req.body.assistido) {
     res.status(400).send({message: 'Filme invalido! Por favor preencha todas as informações.'});
     return
   }
 
-  const filme = req.body;
+  const filme = filmesService.addFilme(req.body);
 
-  await filmesService.addFilme(filme)
-  .then(() => {
-    res.send({message: `O filme ${filme.nome} foi cadastrado com sucesso!`})
-  })
-  .catch((err) => {
-    comsole.error(err);
-    res.status(500).send({error: 'Erro no servidor!'});
-  });
+  if(filme.id) {
+    res.send({message: `O filme ${filme.nome} foi cadastrado com sucesso!`});
+  } else {
+    res.status(500).send({error: 'Erro no servidor!' });
+  }
 }
 
 const editFilme = async (req, res) => {
-  if(!req.body || !req.body.nome || !req.body.iamgem || !req.body.genero || !req.body.nota || !req.body.assistido) {
+  if(!req.body || !req.body.nome || !req.body.imagem || !req.body.genero || !req.body.nota || !req.body.assistido) {
     res.status(400).send({message: 'Não foi possível editar o filme! Por favor preencha todas as informações.'});
     return
   }
 
-  const idParam = req.params.id;
-  const filmeEdit = req.body;
-  await filmesService.editFilme(idParam, filmeEdit)
-  .then(() => {
-    res.send({message: `O filme ${filmeEdit.nome} foi editado com sucesso!`})
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send({error: 'Erro no servidor!'});
-  });
+  const filme = req.body;
+  const edicao = filmesService.editFilme(req.params.id, filme);
+
+  if(edicao) {
+    res.send({message: `O filme ${filme.nome} foi editado com sucesso!`});
+  } else {
+    res.status(404).send({error: 'Erro no servidor! Id não encontrado.'});
+  }
 }
 
 const deleteFilme = async (req, res) => {
-  await filmesService.deleteFilme(req.params.id)
-  .then (() => {
-    res.send({message: `O filme foi excluido com sucesso!`});
-  })
-  .catch((err) => {
-    res.status(500).send({error: 'Erro no servidor!'});
-  });
+  const filme = filmesService.deleteFilme(req.params.id);
+
+  if(filme) {
+    res.send({message: `O filme ${filme.nome} foi excluído com sucesso!`});
+  } else {
+    res.status(404).send({error: 'Erro no servidor! Id não encontrado.'});
+  }
 }
 
 module.exports = {
