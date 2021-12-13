de3f634cc3c689b95b52a3deb766498bab4045c5
Gabriel Matos <g.matos@outlook.com>
2021-12-12 21:24:28 -0300

    edição array de filmes

--- services/filmes.service.js
+++ services/filmes.service.js
@@ -4,7 +4,7 @@ const listaFilmes = [
     nome: "Donnie Darko",
     imagem: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/9494-cartaz.jpg",
     genero: "Ficção científica/Fantasia",
-    nota: 10,
+    nota: 5,
     assistido: false
   },
   {
@@ -12,7 +12,7 @@ const listaFilmes = [
     nome: "O Hobbit: A Batalha dos Cinco Exércitos",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/0/0e/The_Hobbit_-_The_Battle_of_the_Five_Armies.jpg/240px-The_Hobbit_-_The_Battle_of_the_Five_Armies.jpg",
     genero: "Fantasia/Aventura",
-    nota: 10,
+    nota: 5,
     assistido: false
   },
   {
@@ -20,7 +20,7 @@ const listaFilmes = [
     nome: "Liga da Justiça de Zack Snyder",
     imagem: "https://media.fstatic.com/as43yaqgTL57KcvpzqPT4b4e8J4=/210x312/smart/media/movies/covers/2021/03/1615392114_liga-da-justica-de-zack-snyder-filme-poster.jpg",
     genero: "Ação/Aventura",
-    nota: 10,
+    nota: 5,
     assistido: false
   },
   {
@@ -28,7 +28,7 @@ const listaFilmes = [
     nome: "Star Wars: Episódio IV – Uma Nova Esperança",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/7/7e/Star_Wars_Epis%C3%B3dio_IV_Uma_Nova_Esperan%C3%A7a.jpg",
     genero: "Ficção científica/Fantasia",
-    nota: 10,
+    nota: 5,
     assistido: false
   },
   {
@@ -36,7 +36,15 @@ const listaFilmes = [
     nome: "As Branquelas",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/d/de/White_chicks.jpeg/200px-White_chicks.jpeg",
     genero: "Comédia",
-    nota: 10,
+    nota: 5,
+    assistido: false
+  },
+  {
+    id: 6,
+    nome: "Matrix",
+    imagem: "https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG",
+    genero: "Ficção científica/Ação",
+    nota: 5,
     assistido: false
   }
 ]
